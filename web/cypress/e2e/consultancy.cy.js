cd4b99f2f4be505fdb753d248de851843bdc1a3d
Higor <higordamascenostella@gmail.com>
2026-04-08 20:08:24 -0300

    Ajustando

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -27,22 +27,22 @@ describe('Formulário de consultoria', () => {
             .find('select')
             .select('In Company')
 
-        cy.contains('label','Pessoa Física')
-            .should('have.text','Pessoa Física')
+        cy.contains('label', 'Pessoa Física')
+            .should('have.text', 'Pessoa Física')
             .find('input')
             .check()
 
-         cy.contains('label','Pessoa Jurídica')
+        cy.contains('label', 'Pessoa Jurídica')
             .find('input')
             .should('be.not.checked')
 
-        cy.contains('div','CPF')
-            .should('have.text','CPF')
+        cy.contains('div', 'CPF')
+            .should('have.text', 'CPF')
             .find('input')
             .type('40870196839')
             .should('be.visible')
 
-        
+
         const discoveryChannels = [
             'Instagram',
             'LinkedIn',
@@ -51,27 +51,27 @@ describe('Formulário de consultoria', () => {
             'Indicação de Amigo',
         ]
 
-        discoveryChannels.forEach((channel)=>{
+        discoveryChannels.forEach((channel) => {
 
-             cy.contains('label', channel)
-            .find('input')
-            .check()
-            .should('be.checked')
+            cy.contains('label', channel)
+                .find('input')
+                .check()
+                .should('be.checked')
 
 
         })
 
         cy.get('input[type="file"')
-            .selectFile('./cypress/fixtures/Curriculum Higor 2025V1.pdf', {force: true})
+            .selectFile('./cypress/fixtures/Curriculum Higor 2025V1.pdf', { force: true })
 
-        cy.contains('label','Mais Detalhes')
+        cy.contains('label', 'Mais Detalhes')
             .parent()
             .find('textarea')
             .type('Meu nome é Higor e estou desempregado por forças maiores, e agora estou me lascando sem dinheiro')
-        
-        
-        
-        
+
+
+
+
         const tecnolodia = [
             'Funcional',
             'Regressão',
@@ -79,25 +79,73 @@ describe('Formulário de consultoria', () => {
             'Javascript',
             'Pessoa'
         ]
-        tecnolodia.forEach((tech)=>{
+        tecnolodia.forEach((tech) => {
 
-            cy.contains('label','Tecnologias')
+            cy.contains('label', 'Tecnologias')
                 .parent()
                 .find('input')
                 .type(tech)
                 .type('{enter}')
-                
-                cy.contains('label','Tecnologias')
-                    .parent()
-                    .contains('span',tech)
-                    .should('be.visible')
-                
+
+            cy.contains('label', 'Tecnologias')
+                .parent()
+                .contains('span', tech)
+                .should('be.visible')
 
         })
 
+        cy.contains('label', 'termos de uso')
+            .find('input')
+            .click()
 
+        cy.contains('span', 'Li e aceito os')
+            .parent()
+            .find('input')
+            .check()
+
+        cy.contains('button', 'Enviar formulário')
+            .click()
+            .wait(7000)
+        cy.contains('h3', 'Sucesso!')
+            .should('be.visible')
+
+
+
+
+
+    })
+
+    it.only('Deve verificar os campos obrigatórios', () => {
+
+        cy.goTo('Formulários', 'Consultoria')
+        cy.contains('button', 'Enviar formulário')
+            .click()
+
+        cy.get('input[placeholder="Digite seu nome completo"]')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .and('have.class','text-red-400')
+            .and('have.css','color','rgb(248, 113, 113)')
+
+        cy.get('input[placeholder="Digite seu email"]')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .and('have.class','text-red-400')
+            .and('have.css','color','rgb(248, 113, 113)')
         
-           
+        cy.contains('span','Li e aceito os')
+            .parent()
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .and('have.class','text-red-400')
+            .and('have.css','color','rgb(248, 113, 113)')
+            
+
+
+
 
     })
 
